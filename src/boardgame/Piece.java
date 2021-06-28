15704457d910c66d9e00d666c47a0df73ec84628
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-06-28 11:09:07 -0300

    Staring to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,21 @@
+package boardgame;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	//construtor do board!
+	public Piece(Board board) {
+		this.board = board;
+		position = null; //Uma peça recém criada! 
+	}
+	
+	//Criada do get e não o set, pois é permetido a sua alteração.
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
+
+}

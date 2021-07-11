6b2260893f6f0974f4b87c79a5a85117c71810ff
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-07-11 05:54:56 -0300

    Little improvement in board printing!

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,8 +1,29 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 //Aqui nessa classe construimos o design do tabuleiro
 public class UI {
+	
+	//aqui embaixo estão as cores que vão aparecer no terminal
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
 	public static void printBoard(ChessPiece[][] pieces) {
 		//formar as linhas e colunas
@@ -17,7 +38,7 @@ public class UI {
 	}
 	
 	//imprimir uma peça
-	private static void printPiece(ChessPiece piece) { //printPiece recebendo ChessPiece
+	/*private static void printPiece(ChessPiece piece) { //printPiece recebendo ChessPiece
 		if (piece == null) {
 			System.out.print("-");
 		}
@@ -25,6 +46,22 @@ public class UI {
 			System.out.print(piece);
 		}
 		System.out.print(" ");
+	}*/
+	
+	//Agora faremos de outra maneira
+	private static void printPiece(ChessPiece piece) {
+	if (piece == null) {
+        System.out.print("-");
+    }
+    else {
+        if (piece.getColor() == Color.WHITE) {
+            System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+        }
+        else {
+            System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+        }
+    }
+    System.out.print(" ");
 	}
 
 }

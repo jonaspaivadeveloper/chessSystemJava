bd96aeff3e15c7bd7172ed1f226250606e1ecf4e
Jonas Silva <JonasSilvadePaivaDeveloper@gmail.com>
2021-07-20 16:48:22 -0300

    Pawn!

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,83 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece {
+
+	public Pawn(Board board, Color color) {
+		super(board, color);
+	}
+	
+	//Métod abstrato
+	@Override
+	public boolean[][] possibleMoves() {
+		//matriz auxiliar e uma variável p auxiliar 
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		//Movimento da peça branca
+		if(getColor() == Color.WHITE) {
+			p.setValues(position.getRow() - 1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+			//Agora para pular duas linhas para frente
+			p.setValues(position.getRow() - 2, position.getColumn());
+			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+			//Movimento lateral esquerda
+			p.setValues(position.getRow() - 1, position.getColumn() - 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+			//Movimento lateral direita
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+		}
+		
+		//Para as peças pretas
+		else {
+			p.setValues(position.getRow() + 1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+			//Agora para pular duas linhas para frente
+			p.setValues(position.getRow() + 2, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+			//Movimento lateral esquerda
+			p.setValues(position.getRow() + 1, position.getColumn() - 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+			//Movimento lateral direita
+			p.setValues(position.getRow() + 1, position.getColumn() + 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+		}
+		return mat;
+	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
+}
